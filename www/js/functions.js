eec447b4ba99ac357f24d8a992f75bce287b149a
cevau <cvb@sxmail.de>
2016-06-01 12:29:59 +0200

    Anpassungen index.html für JS

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
+$("document").ready(function () {
     var postUrl = "http://pb.ingamelandscapes.de/query.php";
+    alert("script loaded");//debug
 
 
-    $("#login-btn").on("click", function () {
+    $(".log-bwn").on("click", function () {
 
-        var loginStr = $("#inputUser").val();
-        var passwordStr = $("#inputPassword").val();
+        var loginStr = $("#username").val();
+        var passwordStr = $("#password").val();
 
-        //Daten die gesendet werden müssen für die funktion "loginuser"
+        //Daten die gesendet werden mï¿½ssen fï¿½r die funktion "loginuser"
         //Vergleiche mit quellcode von: http://pb.ingamelandscapes.de/
 
-        var sendData = {action: "loginuser", username: loginStr, password: passwordStr}; //Array 
+        var sendData = {action: "loginuser", username: loginStr, password: passwordStr}; //Array
 
         $.ajax({
             url: postUrl,
@@ -18,21 +19,21 @@ $(document).ready(function () {
             data: sendData,
             success: function (data, textStatus, jqXHR)
             {
-                //data ist die rückgabe vom server, zurückgegeben wird ein JSON Array (bestimmte art der Formatierung)
-                //man kann dann einfach drauf zugreifen z.b via returnData.status (gibt das Array zurück mit dem Inhalt "status" und "userInfo")
-                //returnData.status.status ist entweder "ok" oder "error" was genau für antworten kommen kannst du auch oben auf der testseite ausprobieren und danach dann die funktionen schreiben
+                //data ist die rï¿½ckgabe vom server, zurï¿½ckgegeben wird ein JSON Array (bestimmte art der Formatierung)
+                //man kann dann einfach drauf zugreifen z.b via returnData.status (gibt das Array zurï¿½ck mit dem Inhalt "status" und "userInfo")
+                //returnData.status.status ist entweder "ok" oder "error" was genau fï¿½r antworten kommen kannst du auch oben auf der testseite ausprobieren und danach dann die funktionen schreiben
 
                 var returnData = JSON.parse(data);
 
-                //Ist der login ok? 
+                //Ist der login ok?
 
                 if (returnData.status.status === "ok") {
                     //Login ist richtig
-                    //hier müssen wir jetzt der App sagen, dass sie die ID speichern soll damit wir auf jeder "Seite" wissen, dass der User eingeloggt ist
-                    /*                    
+                    //hier mï¿½ssen wir jetzt der App sagen, dass sie die ID speichern soll damit wir auf jeder "Seite" wissen, dass der User eingeloggt ist
+                    /*
                      speichern:
                      localStorage.setItem("pref1", "val1");
-                     
+
                      lesen:
                      var pref1 = localStorage.getItem("pref1");
                      */
@@ -40,21 +41,21 @@ $(document).ready(function () {
                     localStorage.setItem("USERID", returnData.userInfo.id);
 
 
-                    // !!! eigentlich müssten wir hier jetzt testen ob der user bereits in einem team ist und  schon eine klasse hat wenn nicht, dann weiterleitung an die jeweilige seite, wenn doch dann leiten wir direkt zur profilübersicht weiter
-                    
+                    // !!! eigentlich mï¿½ssten wir hier jetzt testen ob der user bereits in einem team ist und  schon eine klasse hat wenn nicht, dann weiterleitung an die jeweilige seite, wenn doch dann leiten wir direkt zur profilï¿½bersicht weiter
+
                     if(returnData.userInfo.team === "0"){
-                        //user hat noch kein team gewählt, weiterleitung zur team seite
-                        alert("Team wählen!");
+                        //user hat noch kein team gewï¿½hlt, weiterleitung zur team seite
+                        alert("Team wÃ¤hlen!");
                     }else if(returnData.userInfo.class === "0"){
-                        //user hat noch keine klasse gewählt, weiterleitung zur klassen seite
-                        alert("Klasse wählen!");
+                        //user hat noch keine klasse gewï¿½hlt, weiterleitung zur klassen seite
+                        alert("Klasse wÃ¤hlen!");
                     }else{
-                        //user hat bereits alles gewählt weiterleitung zum profil
+                        //user hat bereits alles gewï¿½hlt weiterleitung zum profil
                         alert("Anmeldung abgeschlossen!");
                     }
 
-                    //hier jetzt zum testen und weil ich nicht weis wie genau und wann man team und klasse wählt leiten wir einfach an irgendeine seite weiter
-                    //auf allen seiten  außer der login seite müsste am anfang bevor alles andere kommt erst mal abgefragt werden ob bereits ein user eingeloggt ist ich denke hier reicht die abfrage ob im localStorage die USERID gesetzt ist oder nicht, wenn ja kennen wir ja den user und können alle funktionen mit diesder ID abrufen
+                    //hier jetzt zum testen und weil ich nicht weis wie genau und wann man team und klasse wï¿½hlt leiten wir einfach an irgendeine seite weiter
+                    //auf allen seiten  auï¿½er der login seite mï¿½sste am anfang bevor alles andere kommt erst mal abgefragt werden ob bereits ein user eingeloggt ist ich denke hier reicht die abfrage ob im localStorage die USERID gesetzt ist oder nicht, wenn ja kennen wir ja den user und kï¿½nnen alle funktionen mit diesder ID abrufen
                     window.location = "faction.html";
 
                 } else {
@@ -67,5 +68,50 @@ $(document).ready(function () {
                 alert("Connection failed");
             }
         });
-    });
+    });//No register-btn
+    /*$("#register-btn").click(function(){
+      var loginStr = $("#inputUser").val();
+      var passwordStr = $("#inputPassword").val();
+
+      var sendData = {action: "createuser", username: loginStr, password: passwordStr}; //Array
+      $.ajax({
+        url: postUrl,
+        type: "POST",
+        data: sendData,
+        success: function, (data, textStatus, jqXHR)
+        {
+          $.ajax({
+            url: postUrl,
+            type: "POST",
+            data: sendData,
+            success: function (data, textStatus, jqXHR)
+            {
+                var returnData = JSON.parse(data);
+                if (returnData.status.status === "ok") {
+                  localStorage.setItem("USERID", returnData.userInfo.id);
+                  if(returnData.userInfo.team === "0"){
+                        alert("Team wï¿½hlen!");
+                  }else if(returnData.userInfo.class === "0"){
+                        alert("Klasse wï¿½hlen!");
+                  }else{
+                        alert("Anmeldung abgeschlossen!");
+                  }
+                  window.location = "faction.html";
+
+                } else {
+                  alert(returnData.status.status + ": " + returnData.status.message);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert("Connection failed");
+            }
+          });
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+          alert("Connection failed");
+        }
+      })
+    })*/
 });
